9fbff08f1af4a83a110e80d075c6e8d0a05af031
OLindfors <46961512+OLindfors@users.noreply.github.com>
2019-01-23 15:09:49 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+
+<html>
+
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>
-    <link rel="stylesheet" href="style.css">
+    <title>PHP code examples</title>
 </head>
+
 <body>
-    <h1>R0315 - Web App Development with PHP/SQL - Code for Hands on Exercises</h1>
-    <?php
-    /**
-     * Created by PhpStorm.
-     * User: MEILOHI
-     * Date: 23.11.2018
-     * Time: 10.34
-     */
-        $dir    = getcwd (  );
-        $files = scandir($dir, 1);
-        $html = '<ul>';
-        foreach ($files as $key => $value) {
-            if(is_dir($value) && substr($value, 0,1 )!=".") {
-                $html .= '<li><a href="' . $value . '">' . $value . '</a></li>';
-            }
-        }
-        $html .= '</ul>';
-        echo $html;
-    ?>
+    <h1>PHP code examples </h1>
+
+	<div>	
+			<a href="helloworld.php" >Hello World</a>
+			<br>
+			<a href="basicinput.php" >Basic input</a>
+			<br>
+			<a href="basicmath.php" >Basic Math</a>
+			<br>
+			<a href="whileloop.php" >While loop</a>
+			<br>
+			<a href="forloop.php" >For loop</a>
+	</div>
+
 </body>
+
 </html>
